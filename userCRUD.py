fea7abde315d4d37774feeaffb1c64315e3c5d5f
Megan Lee <74307093+meg2lee@users.noreply.github.com>
2020-12-12 23:39:37 +0900

    Update userCRUD.py
    function updated

--- userCRUD.py
+++ userCRUD.py
@@ -1,14 +1,33 @@
 def read_file():
     info = input("회원번호,이름,전화;")
     num,name,phone = info.split(',')
+    print(Member(num,name,phone))
     
-    
-def add_info():
-    
+def add(m):
+    with open('members.csv','a') as file_object:
+        file_object.write(f'{m.num},{m.name},{m.phone}\n')
+        
+def search_mem(num):
+    with open('members.csv','a') as file_object2:
+        for m in file_object2:
+            mnum,name,phone = n.split(',')
+            if(num==mnum):
+                print(m)
 
 while True:
-    action = input("추가(a),검색(s)")
+    action = input("추가(a),검색(f),취소(x)")
     
-    if(action=='a'):
+    if(action=='a'):        
+        add(read_file())
         
-        with open('members.csv','a') as file_object:
+    elif(action=='f'):
+        num = int(input("검색할 회원번호:"))
+        search_mem(num)
+        
+    elif(action=='x'):
+        break
+
+print('프로그램 종료...')
+
+
+    
